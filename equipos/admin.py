afdf4feaf79e4feb4631886bb1aeed8b272004f7
Dannjader <dannjader@gmail.com>
2023-06-28 17:34:01 -0500

    Actualizacion de resgitro de modelos

--- equipos/admin.py
+++ equipos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Equipos, Trabajos
 
 # Register your models here.
+@admin.register(Equipos)
+class EquiposAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Trabajos)
+class TrabajosAdmin(admin.ModelAdmin):
+    pass
